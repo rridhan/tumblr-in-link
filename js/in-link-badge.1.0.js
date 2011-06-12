7684cd1f48fd39c2a62c02ab425b45005e4d760c
me <me@gayspirit.me>
2011-06-12 19:27:17 +0000

    Initiale Release

--- js/in-link-badge.1.0.js
+++ js/in-link-badge.1.0.js
@@ -80,7 +80,7 @@
         '<div class="inlinkbadge" id="badge-'+config.url+'">' +
             '<div id="inlinkbadge-loading">Loading Tumblr In Link Badge...</div>' +
             '<div id="inlinkbadge-title"></div>'+
-            '<ul class="" id="list-'+config.url+'"></ul>' +
+            '<ul class="" id="inlinkbadge-list"></ul>' +
             '<div id="inlinkbadge-logo"><a href="http://tech.gayspirit.me/in-link" title="In-Link Badge Widget"><img src="http://tumblr-in-link.googlecode.com/svn/trunk/img/in-link-sm.png" alt="Tumblr In-Link"></a></div>' +
         '</div>'
     );
@@ -129,7 +129,7 @@
 				{ if(config.num--<=0) return;
                 
                     var item='<li class="inlinkbadge-item" id="'+types[i]+'"><a class="inlinkbadge-link" href="'+links[i]+'" title="'+titles[i]+'"><img src="'+images[i]+'" alt="'+titles[i]+'"><p>'+titles[i]+'</p></a></li>';
-                    $j('"#inlinkbadge-list-'+config.url+'"').append(item);
+                    $j("#inlinkbadge-list").append(item);
                 }
             }
             $j("#inlinkbadge-title").html('<h2>'+config.title+'</h2>');
