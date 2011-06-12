bbfb6811e4b481c29a7a90c763d44335dfdd0e9d
me@gayspirit.me <me@gayspirit.me@d3502e26-0ea7-81f1-5e03-df638b5bd559>
2011-06-12 19:49:29 +0000

    Initiale Release

--- trunk/js/in-link-badge.1.0.js
+++ trunk/js/in-link-badge.1.0.js
@@ -94,7 +94,7 @@
             {req=$j.getJSON(url_base+'api/read/json?callback=?&num='+config.num+'&filter=text', function(data) {
             		$j(data.tumblelog).each(function(i, tumblelog) {
             			ttitle.push(tumblelog['title']); 
-                        tdescr.push(post['description']);
+                        tdescr.push(tumblelog['description']);
                         if(tdescr.length>config.len){ tdescr=tdescr.slice(0,config.len); text+='...';}
                     });
             
