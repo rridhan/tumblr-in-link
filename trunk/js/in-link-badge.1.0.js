658667faa430e03ee01b93a38cbfc805ad97f998
me@gayspirit.me <me@gayspirit.me@d3502e26-0ea7-81f1-5e03-df638b5bd559>
2011-06-12 17:36:29 +0000

    Initiale Release

--- trunk/js/in-link-badge.1.0.js
+++ trunk/js/in-link-badge.1.0.js
@@ -85,7 +85,7 @@
         function getBadge() {
             var req;
             for(var i=0; i<url_base.length; i++){
-                req=$j.getJSON(url_base+'api/read/json?callback=?&filter=text&num='+config.num+'&start=0&type='+config.type+'', function(data) {
+                req=$j.getJSON(url_base+'api/read/json?callback=?&num='+config.num+'&start=0&type='+config.type+'&filter=text', function(data) {
                     $j(data.posts).each(function(i, post) {
                         var text='';
                         if(post.type=='regular') text+=post['regular-title'];
