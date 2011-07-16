4720c63fd8b74de1a2c0a99db5b46a11dd3eb6a4
me@gayspirit.me <me@gayspirit.me@d3502e26-0ea7-81f1-5e03-df638b5bd559>
2011-07-16 13:06:06 +0000

    Initiale Release

--- trunk/js/tumblr-in-link.2.0.js
+++ trunk/js/tumblr-in-link.2.0.js
@@ -115,7 +115,7 @@
                         images.push(image);
                         types.push(post['type'])
                     });
-                    
+                    }
                 }).complete(getList);
             }
             
