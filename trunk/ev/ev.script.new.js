ff8bf427212290fb7942593253dd1ce0568d83ed
me@gayspirit.me <me@gayspirit.me@d3502e26-0ea7-81f1-5e03-df638b5bd559>
2013-06-02 07:27:19 +0000

    ccc

--- trunk/ev/ev.script.new.js
+++ trunk/ev/ev.script.new.js
@@ -141,7 +141,7 @@ $(function () {
             }
             var evX = (M - evAX) / 3; // Assegna 100px all'header, dividi il resto dello schermo in terzi.
             var evXX = (evX * 2); // Assegna due terzi all'area immagini
-            var Z = (evXX - U) / 2;
+            var Z = (evXX - U) / 3;
             D.css("margin-top", Z).fadeTo(200, 1)
         }
 
